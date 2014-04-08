c803bd00a78f4a8aad2dae2a593f4450cfafa4ab
Jure Slak <jure.slak@gmail.com>
2014-04-08 13:49:03 +0200

    Added necessary files.

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import bottle
-from beaker.middleware import SessionMiddleware
+from bottle.ext import sqlite
 
-session_opts = {
-    'session.type': 'file',
-    'session.cookie_expires': 300,
-    'session.data_dir': './data',
-    'session.auto': True
-}
-app = SessionMiddleware(bottle.app(), session_opts)
+app = bottle.Bottle()
+plugin = sqlite.Plugin(dbfile='/tmp/test.db')
+app.install(plugin)
 
-@bottle.route('/test')
-def test():
-  s = bottle.request.environ.get('beaker.session')
-  s['test'] = s.get('test',0) + 1
-  s.save()
-  return 'Test counter: %d' % s['test']
+@app.route('/show/:item')
+def show(item, db):
+    row = db.execute('SELECT * from items where name=?', item).fetchone()
+    if row:
+        return template('showitem', page=row)
+    return HTTPError(404, "Page not found")
 
 bottle.run(app=app)
