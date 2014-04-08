e6576db567fb081c8fb7342ad60581757c4a4f62
Jure Slak <jure.slak@gmail.com>
2014-04-08 15:14:03 +0200

    Added database script.

---
+++ baza.py
@@ -0,0 +1,65 @@
+import sqlite3
+conn = sqlite3.connect('baza.db')
+cur = conn.cursor()
+
+try: cur.execute("DROP TABLE uporabniki");
+except: pass
+try: cur.execute("DROP TABLE ankete");
+except: pass
+try: cur.execute("DROP TABLE vprasanja");
+except: pass
+try: cur.execute("DROP TABLE odgovori");
+except: pass
+
+cur.execute("""CREATE TABLE uporabniki (
+                    username TEXT PRIMARY KEY,
+                    password TEXT,
+                    email TEXT
+                    );""")
+
+cur.execute("""CREATE TABLE ankete (
+                    id INTEGER PRIMARY KEY,
+                    naslov TEXT,
+                    uvod TEXT,
+                    uporabnik TEXT,
+                    FOREIGN KEY(uporabnik) REFERENCES uporabniki(username));""")
+
+cur.execute("""CREATE TABLE vprasanja (
+                    id INTEGER PRIMARY KEY,
+                    text TEXT,
+                    anketa INTEGER,
+                    FOREIGN KEY(anketa) REFERENCES ankete(id));""")
+
+cur.execute("""CREATE TABLE odgovori (
+                    id INTEGER PRIMARY KEY,
+                    skupina INTEGER,
+                    text TEXT,
+                    vprasanje INTEGER,
+                    FOREIGN KEY(vprasanje) REFERENCES vprasanja(id));""")
+
+cur.execute("INSERT INTO uporabniki VALUES ('jureslak','jureslak', 'mail')")
+
+cur.execute("INSERT INTO ankete VALUES (NULL, 'Moja anketa', 'bla bla', 'jureslak')")
+
+cur.execute("INSERT INTO vprasanja VALUES (NULL, 'Starost?', 0)")
+cur.execute("INSERT INTO vprasanja VALUES (NULL, 'Visina?', 0)")
+cur.execute("INSERT INTO vprasanja VALUES (NULL, 'Teza?', 0)")
+
+cur.execute("INSERT INTO odgovori VALUES (NULL, 0, '20', 0)")
+cur.execute("INSERT INTO odgovori VALUES (NULL, 0, '180', 1)")
+cur.execute("INSERT INTO odgovori VALUES (NULL, 0, '70', 2)")
+
+cur.execute("INSERT INTO odgovori VALUES (NULL, 1, '25', 0)")
+cur.execute("INSERT INTO odgovori VALUES (NULL, 1, '140', 1)")
+cur.execute("INSERT INTO odgovori VALUES (NULL, 1, '30', 2)")
+
+for line in cur.execute("SELECT * FROM uporabniki"):
+    print(line)
+for line in cur.execute("SELECT * FROM vprasanja"):
+    print(line)
+for line in cur.execute("SELECT * FROM ankete"):
+    print(line)
+for line in cur.execute("SELECT * FROM odgovori"):
+    print(line)
+
+conn.commit()   
