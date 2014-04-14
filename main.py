c180bef97288dbeae1c047647b348e0db8e7dd98
Jure Slak <jure.slak@gmail.com>
2014-04-15 00:17:55 +0200

    Added beaker session.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import bottle
 from bottle import view, route, run, Bottle, install
 from bottle_sqlite import SQLitePlugin
+from beaker.middleware import SessionMiddleware
 
 plugin = SQLitePlugin(dbfile='baza.db', keyword='db')
 install(plugin)
+
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 300,
+    'session.data_dir': './session',
+    'session.auto': True
+}
+
+app = SessionMiddleware(bottle.app(), session_opts)
+
 
 @route('/anketa/<uid:int>/', template='anketa')
 #  @view('anketa')
@@ -13,8 +25,15 @@ def show_anketa(uid, db):
         abort(404)
     return {'vprasanja': list(result)}
 
+@route('/count/')
+def count():
+    s = bottle.request.environ.get('beaker.session')
+    s['test'] = s.get('test',0) + 1
+    s.save()
+    return 'Test counter: %d' % s['test']
+
 @route('/')
 @view('index')
 def index(): return
 
-run()
+run(app=app)
