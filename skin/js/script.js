991a1fa53e0dc66e7a281e89a7632788841fbdc7
roklekse <kren470@gmail.com>
2014-04-20 01:13:11 +0200

    urejanje vprasanj

--- skin/js/script.js
+++ skin/js/script.js
@@ -1,9 +1,22 @@
 $(document).ready( function(){
-  $("#urejanje_ozadje").click( function(){ zapri();});
+  $("#urejanje_ozadje").click( function(e){
+    if (e.target == this){
+      zapri();
+    }
+  });
 });
 
 function uredi(n){
   $("#urejanje_ozadje").show();
+  $("#id_vprasanje").text(n);
+  tip = $("#vprasanje"+n).attr("data-tip");
+  switch (tip){
+    case "text":
+      htmlString = '<form action=""> Vprasanje: <input type="text" value="'+$("#vprasanje"+n+"_text").text()+'"/> </form>';
+      break;
+  
+  }
+  $("#urejanje_placeholder").html(htmlString);
 }
 
 function zapri(){
