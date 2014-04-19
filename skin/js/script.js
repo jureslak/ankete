f50d2e6cb8ccca789f3130497e3f666bdc47d481
roklekse <kren470@gmail.com>
2014-04-19 22:19:10 +0200

    Create script.js

---
+++ skin/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready( function(){
+  $("#urejanje_ozadje").click( function(){ zapri();});
+});
+
+function uredi(n){
+  $("#urejanje_ozadje").show();
+}
+
+function zapri(){
+  $("#urejanje_ozadje").hide();
+}
