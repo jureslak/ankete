c18627ffef3927b6892789f56a8f71264c6e8b31
roklekse <kren470@gmail.com>
2014-04-21 00:06:08 +0200

    dinamicen textarea + ostalo

--- skin/js/script.js
+++ skin/js/script.js
@@ -4,21 +4,39 @@ $(document).ready( function(){
       zapri();
     }
   });
+  $("#tip").change( function(){
+    if ($("#tip").val() == "text"){
+      if ($("#seznam").css("display")!="none") $("#seznam").hide(200);//.css("display","none");
+    }
+    else if ($("#tip").val() == "radiobutton"){
+      if ($("#seznam").css("display")=="none") callbackPrikazSeznam();
+    }
+  });
 });
 
 function uredi(n){
   $("#urejanje_ozadje").show();
   $("#id_vprasanje").text(n);
+  //dinamicno izbiranje pravega <optiona> v <selectu> tip
   tip = $("#vprasanje"+n).attr("data-tip");
-  switch (tip){
-    case "text":
-      htmlString = '<form action=""> Vprasanje: <input type="text" value="'+$("#vprasanje"+n+"_text").text()+'"/> </form>';
-      break;
+  $("#tip > option").each( function(){
+    if (this.text == tip) $(this).prop("selected", true);
+  });
+  //dinamicno dodajanje vrednosti "naslov"
+  $("#naslov").val($("#vprasanje"+n+"_text").text());
   
+  if (tip == "radiobutton"){
+    callbackPrikazSeznam();
+    $("#seznam").html($("#vprasanje"+n).attr("data-vprasanja"));
   }
-  $("#urejanje_placeholder").html(htmlString);
+}
+
+function callbackPrikazSeznam()
+{
+  $("#seznam").show(200);
 }
 
 function zapri(){
   $("#urejanje_ozadje").hide();
+  $("#seznam").css("display","none");
 }
